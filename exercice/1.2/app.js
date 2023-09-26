5dfef9e3f6212252a7b6da9fad037066920ec5ed
youssaf-elhilali-vinci <128787706+youssaf-elhilali-vinci@users.noreply.github.com>
2023-09-26 23:19:40 +0200

    1.2 : app middleware

--- exercice/1.2/app.js
+++ exercice/1.2/app.js
@@ -5,8 +5,39 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var filmsRouter = require('./routes/films');
 
 var app = express();
+
+
+let cptGet = 0;
+let cptGetFilms = 0;
+let cptPost = 0;
+let cptDelete = 0;
+app.use((req, res, next) => {
+    const {method, path} = req; // methode sont toute les methode utilisé exemple GET, POST,... | path le chemin url | ce qui est renvoyer
+    
+    console.log(method);
+    console.log(path);
+    
+    if(method == "GET" && path == "/"){
+        cptGet++;
+        console.log("counter GET / : " + cptGet)
+    }
+    if(method == "GET" && path == "/films"){
+        cptGetFilms++;
+        console.log("counter GET /films : " + cptGetFilms)
+    }
+    if(method == "POST" && path == "/films"){
+        cptPost++;
+        console.log("counter POST /films: " + cptPost)
+    }
+    if(method == "DELETE" && path == "/films"){
+        cptDelete++;
+        console.log("counter DELETE /films: " + cptDelete)
+    }
+    next(); //passe au suivant
+});
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -16,5 +47,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/films', filmsRouter);
 
 module.exports = app;
