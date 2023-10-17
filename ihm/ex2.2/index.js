fd48822d06528cf54c154c49ec46474bf9d3ac37
sszkethzedfn <skdfn>
2023-10-17 11:24:45 +0200

    2.2 : HMI : dom interaction

--- ihm/ex2.2/index.js
+++ ihm/ex2.2/index.js
@@ -1,10 +1,14 @@
-const nbre = 0 ;
+let nbre = 0 ;
 const body = document.querySelector("body");
+const btn1 = document.querySelector("h1");
+const h2 = document.querySelector("h2");
 
 body.addEventListener('click', () => {
-    btn1.innerText = 'myBtn1 : I have been clicked !';
-    if (nbre<=9 && nbre >= 5 ) return "Bravo, bel échauffement !"
-    if ( nbre>10 ) return "Vous êtes passé maître en l'art du clic !"
+    console.log("efheifuehifuhef")
+    nbre++;
+    
+    btn1.innerText = 'myBtn1 : I have been clicked : ' + nbre;
+    if (nbre<=9 && nbre >= 5 ){ h2.innerText = "Bravo, bel échauffement !"}
+    if ( nbre>10 ){ h2.innerText = "Vous êtes passé maître en l'art du clic !"}
   });
   
-alert("")
