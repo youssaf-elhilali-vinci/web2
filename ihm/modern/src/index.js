c91a919858cf9e38130e1a19027569947d1dfa4f
youssaf-elhilali-vinci <128787706+youssaf-elhilali-vinci@users.noreply.github.com>
2023-11-16 11:28:11 +0100

    2.8: dynamic HomePage.

--- ihm/modern/src/index.js
+++ ihm/modern/src/index.js
@@ -1,2 +1,26 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './stylesheets/main.css';
+
+import FC24 from "./img/fc24_image.jpg"
+import SNOWFALL from "./img/snowfall.jpg"
+
+
+const main = document.querySelector('main');
+
+const homePage =  `
+    <div class="text-center">
+        <h1>Mon jeu favori</h1>
+        <div>
+            <img src="${FC24}" alt="photo du jeu fc 24" width=50% height=50%/>
+        </div>
+
+
+        <h1>Ma série favori</h1>
+
+        <div>
+            <img src="${SNOWFALL}" alt="photo de la série snowfall" width=50% height=50%/>
+        </div>
+<div>
+`
+
+main.innerHTML += homePage 
