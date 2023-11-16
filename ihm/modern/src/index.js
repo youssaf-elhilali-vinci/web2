4a9517db500130dd3e485a82f04e6a5359262cf3
youssaf-elhilali-vinci <128787706+youssaf-elhilali-vinci@users.noreply.github.com>
2023-11-16 19:04:12 +0100

    2.9: dynamic page content

--- ihm/modern/src/index.js
+++ ihm/modern/src/index.js
@@ -1,26 +1,66 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './stylesheets/main.css';
+    import 'bootstrap/dist/css/bootstrap.min.css';
+    import './stylesheets/main.css';
 
-import FC24 from "./img/fc24_image.jpg"
-import SNOWFALL from "./img/snowfall.jpg"
+    import FC24 from "./img/fc24_image.jpg"
+    import SNOWFALL from "./img/snowfall.jpg"
 
 
-const main = document.querySelector('main');
+    const mainWrapper = document.querySelector('main');
 
-const homePage =  `
-    <div class="text-center">
-        <h1>Mon jeu favori</h1>
-        <div>
-            <img src="${FC24}" alt="photo du jeu fc 24" width=50% height=50%/>
+    renderToHomePage()
+
+
+    function renderToHomePage() {
+        const homePage =  `
+        <div class="container text-center">
+            <div class="row">
+                <div class="col">
+                    <h3>Welcome to myMovies !</h3>
+
+                    <p>Here you can find a selection of my favorite serie and game ; )</p>
+                </div>
+            </div>
+        
+            <div class="row mb-3">
+                <div class="col">
+                    <button id="about" class="btn btn-dark">About</button>
+                </div>
+            </div>
+
+
+            <div class="row">
+                <div class="col-12 col-lg-6">
+                    <img src="${FC24}" alt="photo du jeu fc 24" width=50% height=50%/>
+                </div>
+
+                <div class="col-12 col-lg-6">
+                    <img src="${SNOWFALL}" alt="photo de la série snowfall" width=50% height=50%/>
+                </div>
+            </div>
         </div>
+    `
+        mainWrapper.innerHTML = homePage
+
+        const btnForAbout = document.getElementById('about')
+        btnForAbout.addEventListener("click", renderToAboutPage)
+        
+    
+    }
+   
+
+    function renderToAboutPage() {
+        
+        const backPage = `<h1>L'auteur de cette page est Youssaf El hilali, 
+        étudiant dans une haute école dénomé Institut Léonard de Vinci</h1>
+        <button id="back">Back</button>
+        `
+        mainWrapper.innerHTML = backPage
+        
+        const btnForBack = document.getElementById('back')
+        btnForBack.addEventListener("click", renderToHomePage)
+
+    }
 
 
-        <h1>Ma série favori</h1>
 
-        <div>
-            <img src="${SNOWFALL}" alt="photo de la série snowfall" width=50% height=50%/>
-        </div>
-<div>
-`
-
-main.innerHTML += homePage 
+    
