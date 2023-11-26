b12f89874c010f35f68c243c197ec0b7d3dda971
youssaf-elhilali-vinci <128787706+youssaf-elhilali-vinci@users.noreply.github.com>
2023-11-26 01:49:22 +0100

    2.11.2 : add films

--- ihm/ex2.11/src/Components/Pages/AddMoviePage .js
+++ ihm/ex2.11/src/Components/Pages/AddMoviePage .js
@@ -9,15 +9,51 @@ const AddMoviePage  = () => {
 function formToAddMovie(){
     const main = document.querySelector('main')
         main.innerHTML += `
+        <div class="text-center">
             <h2>Add a movie</h2>
 
-            <form id="monFormulaire">
-                <label for="saisie">Add your movie :</label>
-                <input type="text" id="saisie" name="saisie" required>
+            <form>
+                <label for="title">Titre du film:</label>
+                <input class="form-control" type="text" id="title" name="title" required><br>
 
-                <input type="submit" value="Soumettre">
+                <label for="duration">Durée du film (en minutes):</label>
+                <input class="form-control" type="number" id="duration" name="duration" required><br>
+
+                <label for="budget">Budget de production (en millions):</label>
+                <input class="form-control" type="number" id="budget" name="budget" required><br>
+
+                <label for="link">Lien vers la description du film:</label>
+                <input class="form-control" type="url" id="link" name="link" required><br>
+
+                <input type="submit" class="btn btn-primary" value="Add Moovie">
             </form>
-        `
+        </div>
+
+    `
+    const myForm = document.querySelector('form')
+    const title = document.querySelector('#title')
+    const duration = document.querySelector('#duration')
+    const budget = document.querySelector('#budget')
+    const link = document.querySelector('#link')
+
+    myForm.addEventListener('submit', async (e) => {
+        e.preventDefault();
+    
+
+
+    const movieToBeCreated ={
+        title : title.value,
+        duration: Number(duration.value),
+        budget: Number(budget.value),
+        link: link.value,
+    };
+
+    
+    // eslint-disable-next-line no-console
+    console.log("voici le nouveau film : " , movieToBeCreated)
+});
+
+
 }
 
   
